3229b2ee3dda6bdcb856b3e9f8339aca906e33a5
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-02-06 18:10:01 -0300

    Criou página Tarefas e criou Listagem de Tarefas

--- users-crud/src/pages/Tasks/index.tsx
+++ users-crud/src/pages/Tasks/index.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Table } from 'react-bootstrap';
+import api from '../../services/api';
+
+interface ITask {
+  id: number;
+  title: string;
+  description: string;
+  finished: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
 
 const Tasks: React.FC = () => {
-  return <h1>Tasks page</h1>;
+
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  useEffect(() => {
+    loadTasks()
+  }, []);
+
+  async function loadTasks() {
+    const response = await api.get('/tasks');
+    console.log(response);
+    setTasks(response.data);
+  }
+
+  return (
+    <div className="container">
+      <br/>
+      <h1>Tasks page</h1>
+      <br/>
+        <Table striped bordered hover className="text-center">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Data de Nascimento</th>
+              <th>Tipo de Usuário</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                tasks.map(task => (
+                  <tr key={ task.id }>
+                    <td>{ task.id }</td>
+                    <td>{ task.title }</td>
+                    <td>{ task.description }</td>
+                    <td>{ task.created_at }</td>
+                    <td>{ task.finished }</td>
+                    <td>
+                      <Button size="sm">Editar</Button>{' '}
+                      <Button size="sm" variant="danger">Remover</Button>{' '}
+                    </td>
+                  </tr>
+                ))
+
+              }
+          </tbody>
+        </Table>
+    </div>
+  );
 }
 
 export default Tasks;
