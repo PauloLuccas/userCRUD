6c09ff88618192238e141b8e6e7325afbccc14af
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-02-08 15:00:53 -0300

    TasksController Terminada

--- nodeJS/tasks_api/src/routes.ts
+++ nodeJS/tasks_api/src/routes.ts
@@ -1,14 +1,18 @@
 import { Router, Request, Response } from 'express';
 
-import { getTasks, saveTasks } from './controller/TasksController';
+import { getTasks, saveTasks, getTask, updateTasks, finishedTask, removeTask } from './controller/TasksController';
 
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello World! 3'})
+    return response.json({ message: 'Hello World!'})
 });
 
 routes.get('/tasks', getTasks)
+routes.get('/tasks/:id', getTask)
 routes.post('/tasks', saveTasks)
+routes.put('/tasks/:id', updateTasks)
+routes.patch('/tasks/:id', finishedTask)
+routes.delete('/tasks/:id', removeTask)
 
 export default routes;
