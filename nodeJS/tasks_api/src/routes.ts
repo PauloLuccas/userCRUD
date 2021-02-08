6b35142cf642f192e6669cdf3ceebe9d48ad5b30
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-02-08 12:38:08 -0300

    Implementado o nodemon

--- nodeJS/tasks_api/src/routes.ts
+++ nodeJS/tasks_api/src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello World!'})
+    return response.json({ message: 'Hello World! 2'})
 });
 
 export default routes;
